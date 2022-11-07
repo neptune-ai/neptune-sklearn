d69bc6c0c396648a8e77963d9a20e2f770a99496
Rafal Jankowski <rafal.jankowski@neptune.ai>
2022-11-07 12:59:34 +0100

    Moved to Poetry with package building

--- src/neptune_sklearn/impl/__init__.py
+++ src/neptune_sklearn/impl/__init__.py
@@ -34,8 +34,7 @@ __all__ = [
     "get_pickled_model",
     "get_scores",
     "get_test_preds",
-    "get_test_preds_proba",
-    "__version__",
+    "get_test_preds_proba"
 ]
 
 import matplotlib.pyplot as plt
@@ -78,10 +77,6 @@ try:
 except ImportError:
     # neptune-client=1.0.0 package structure
     import neptune
-
-from neptune_sklearn._version import get_versions
-
-__version__ = get_versions()["version"]
 
 
 def create_regressor_summary(regressor, X_train, X_test, y_train, y_test, nrows=1000, log_charts=True):
